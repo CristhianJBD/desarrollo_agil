d48d6ae444c428e8439dea3bc72c4c522485ca74
Cristhian <cristhian.benitez5@gmail.com>
2016-10-28 20:39:32 -0300

    terminacion del diseno y conexion con la api
    Signed-off-by: Cristhian <cristhian.benitez5@gmail.com>

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,37 @@
  * Controller of the desarrolloAgilApp
  */
 angular.module('desarrolloAgilApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AboutCtrl',['$scope','$rootScope','$http' ,function ($scope,$rootScope,$http) {
+
+    $scope.username = $rootScope.username;
+    $scope.token = $rootScope.token;
+    $scope.isAuth =$rootScope.isAuth;
+
+    var Authorization ={"Authorization": "Basic " + $scope.token};
+
+
+    if($scope.isAuth == true){
+      repositorios();
+    }
+
+    function repositorios()  {
+      $http.get('https://api.github.com/users/' + $scope.username +'/repos', {headers: Authorization}).success(function (response) {
+
+        $scope.reposDatos = response;
+        })
+    }
+
+    $scope.buscar = function () {
+      var usuario = document.getElementById('usuario').value;
+      $http.get('https://api.github.com/users/' + usuario, {headers: Authorization}).success(function (response) {
+      $scope.data = response;
+        $scope.username=usuario;
+        repositorios();
+        }, function () {
+        window.alert('¡El usuario buscado no se encuentra registrado en github!');
+        }
+      )
+    }
+
+
+  }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,29 @@
  * Controller of the desarrolloAgilApp
  */
 angular.module('desarrolloAgilApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl',['$scope','$location','$http','$rootScope', function ($scope,$location,$http,$rootScope) {
+
+    $scope.username ="";
+    $scope.password ="";
+
+    $scope.login = function () {
+      var url = 'https://api.github.com/user';
+
+      var token = btoa($scope.username + ":" + $scope.password);
+      var Authorization ={"Authorization": "Basic " + token};
+
+      return $http.get(url,{headers:Authorization}).success(function (response) {
+        $rootScope.data=response;
+        $rootScope.username = $scope.username;
+        $rootScope.token = token;
+        $rootScope.isAuth= true;
+
+        $location.path('/about');
+        console.log(response);
+      },function () {
+        window.alert("Usuario o Contraseña incorrectos");
+        $rootScope.isAuth = false;
+      })
+
+    }
+  }]);
