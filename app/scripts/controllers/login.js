c88c5c3f521048d8d24a3e5af8a430926793d210
Cristhian <cristhian.benitez5@gmail.com>
2016-10-28 09:46:56 -0300

    angular and yeoman
    Signed-off-by: Cristhian <cristhian.benitez5@gmail.com>

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desarrolloAgilApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the desarrolloAgilApp
+ */
+angular.module('desarrolloAgilApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
